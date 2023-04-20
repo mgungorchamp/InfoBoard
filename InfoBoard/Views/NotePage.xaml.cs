5bb7c325331552e190cfed0779b3dcf739d0ede6
Murat Gungor <mgungor@champlain.edu>
2023-04-20 17:30:05 -0400

    Working version

--- InfoBoard/Views/NotePage.xaml.cs
+++ InfoBoard/Views/NotePage.xaml.cs
@@ -1,28 +1,64 @@
+using static Android.Content.ClipData;
+
 namespace InfoBoard.Views;
 
 //https://learn.microsoft.com/en-us/dotnet/maui/tutorials/notes-app/?view=net-maui-7.0&source=docs&tutorial-step=4
+
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
     public NotePage()
 	{
 		InitializeComponent();
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
     }
 
-    private void SaveButton_Clicked(object sender, EventArgs e)
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
+    }
+    public string ItemId {
+        set { LoadNote(value); }
+    }
+
+    private async void SaveButton_Clicked(object sender, EventArgs e)
     {
         // Save the file.
-        File.WriteAllText(_fileName, TextEditor.Text);
+        //File.WriteAllText(_fileName, TextEditor.Text);
+
+        if (BindingContext is Models.Note note)
+            File.WriteAllText(note.Filename, TextEditor.Text);
+
+        await Shell.Current.GoToAsync("..");
     }
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
         // Delete the file.
-        if (File.Exists(_fileName))
-            File.Delete(_fileName);
+        // if (File.Exists(_fileName))
+        //   File.Delete(_fileName);
 
-        TextEditor.Text = string.Empty;
+        //TextEditor.Text = string.Empty;
+
+        if (BindingContext is Models.Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.Filename))
+                File.Delete(note.Filename);
+        }
+
+        await Shell.Current.GoToAsync("..");
     }
 }
