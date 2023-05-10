f5d9c08cf6e641d050126236a4d244f507da07cb
Murat Gungor <mgungor@champlain.edu>
2023-05-10 15:21:23 -0400

    22

---
+++ InfoBoard/ViewModel/ImageViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoBoard.ViewModel
+{
+    partial class ImageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _imageSource;
+        private int _refreshInMiliSecond;
+        private TimeSpan _cachingInterval; //caching interval
+
+        public string ImageSource {
+            get => _imageSource;
+            set {
+                if (_imageSource == value)
+                    return;
+                _imageSource = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TimeSpan Interval {
+            get => _cachingInterval;
+            set {
+                if (_cachingInterval == value)
+                    return;
+                _cachingInterval = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ImageViewModel() 
+        {
+            _cachingInterval = new TimeSpan(0, 0, 10, 00); // TimeSpan (int days, int hours, int minutes, int seconds);
+            _refreshInMiliSecond = 3000;
+            ChangeImage();
+        }
+
+        public async void ChangeImage()
+        {
+            _imageSource = "https://aka.ms/campus.jpg"; //https://picsum.photos/200/300
+            OnPropertyChanged(nameof(ImageSource));
+            await Task.Delay(_refreshInMiliSecond);
+
+            _imageSource = "https://www.champlain.edu/assets/images/Internships/Internships-Hero-Desktop-1280x450.jpg";
+            OnPropertyChanged(nameof(ImageSource));
+            await Task.Delay(_refreshInMiliSecond);
+
+
+            _imageSource = "https://source.unsplash.com/random/1920x1080/?wallpaper,landscape,animals";
+            OnPropertyChanged(nameof(ImageSource));
+            await Task.Delay(_refreshInMiliSecond*3);
+
+            ChangeImage();
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+}
---
+++ InfoBoard/Views/ImageDisplay.xaml.cs
@@ -0,0 +1,11 @@
+using InfoBoard.ViewModel;
+namespace InfoBoard.Views;
+
+public partial class ImageDisplay : ContentPage
+{
+	public ImageDisplay()
+	{
+		InitializeComponent();
+		//BindingContext = new ImageViewModel();
+	}
+}
