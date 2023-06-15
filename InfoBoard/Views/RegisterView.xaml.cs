a5ebecf49892e491673586a5e3b7871baa9bdba3
Murat Gungor <83103466+mgungorchamp@users.noreply.github.com>
2023-06-15 03:07:01 -0400

    Return Button removed from register window

--- InfoBoard/Views/RegisterView.xaml.cs
+++ InfoBoard/Views/RegisterView.xaml.cs
@@ -8,6 +8,8 @@ public partial class RegisterView : ContentPage
 	public RegisterView()
 	{
         InitializeComponent();
+        NavigationPage.SetHasNavigationBar(this, false);
+        NavigationPage.SetHasBackButton(this, false);
         BindingContext = RegisterDeviceViewModel.Instance;
         //attemptRegisteringDevice();
         //updateQrCodeImageAndRegisterDevice();
